fc2e5cfafa7130d69dd780c7a22bd7554420be42
Harshit <harshitbhatt1998@gmail.com>
2019-07-29 16:06:40 +0530

    delete a comment

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ var campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes       = require('./routes/index');
 
 //mongoose.connect("mongodb://localhost/yelp_camp");
-//mongoose.connect("mongodb://harshit:<happy>@cluster0-shard-00-00-tvfpv.mongodb.net:27017,cluster0-shard-00-01-tvfpv.mongodb.net:27017,cluster0-shard-00-02-tvfpv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", {useNewUrlParser: true});
-mongoose.connect("mongodb+srv://harshit:happy@cluster0-tvfpv.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://harshit:happy@cluster0-tvfpv.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
